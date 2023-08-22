79cfe538fe3e4d97f18e28b9d44a835d5733e5ad
Valeri Hristov <valericfbg@gmail.com>
2023-08-22 16:40:28 +0300

    commit the mod.go files

---
+++ html-link-parser/go.mod
@@ -0,0 +1,5 @@
+module example.com/link-spider
+
+go 1.18
+
+require golang.org/x/net v0.14.0 // indirect
---
+++ sitemap/main.go
@@ -0,0 +1,8 @@
+// Ok, I am too lazy to create separate packages for this one...I will code everything here
+package main
+
+import spider "example.com/link-spider"
+
+func main() {
+	spider.Parse()
+}
